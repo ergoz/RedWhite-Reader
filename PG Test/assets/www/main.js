7b0f3d2f69970c0c3cea447710ddbf22efa7980a
Sergey <ergozru@gmail.com>
2012-08-05 16:49:12 +0400

    created repo

---
+++ PG Test/assets/www/main.js
@@ -0,0 +1,44 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+*/
+
+var deviceReady = function() {
+
+};
+
+ function hideSplash() {
+        setTimeout(function() {
+                   navigator.splashscreen.hide();
+                   }, 1000);
+
+    }
+
+var beep = function() {
+    navigator.notification.beep(2);
+};
+
+var vibrate = function() {
+    navigator.notification.vibrate(0);
+};
+
+function init() {
+    // the next line makes it impossible to see Contacts on the HTC Evo since it
+    // doesn't have a scroll button
+    // document.addEventListener("touchmove", preventBehavior, false);
+    document.addEventListener("deviceready", deviceReady, true);
+}
---
+++ PG Test/src/com/ergoz/rwread/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ergoz.rwread;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl("file:///android_asset/www/index.html",5000);
+    }
+
+    
+}
