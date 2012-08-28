3db29201971a47a196efbba9d35bd1f189eb0ed3
Sergey <ergozru@gmail.com>
2012-08-28 18:47:04 +0400

    фиксы и рефакторинг

--- system/main.js
+++ system/main.js
@@ -1,43 +1,28 @@
-var beep = function() {
+function beep() {
     navigator.notification.beep(1);
-};
+}
 
-
-var vibrate = function() {
+function vibrate() {
     navigator.notification.vibrate(1000);
-};
+}
 
-
-var deviceReady = function() {
+function deviceReady() {
 if(navigator.network && navigator.network.connection.type != Connection.NONE) {
-	navigator.notification.vibrate(10);
+	navigator.notification.vibrate(1);
 	} else {
+	beep();
 	navigator.notification.vibrate(1);
 	navigator.notification.vibrate(10);
-	navigator.notification.vibrate(100);
-	navigator.notification.vibrate(1000);
 	}
-};
-
-
-var onMenuKeyDown = function() {
-    // Handle the back button
-	navigator.notification.vibrate(1);
-    navigator.notification.beep(1);
-	navigator.notification.vibrate(1);
-    navigator.notification.beep(1);
-	navigator.notification.vibrate(1);
-	location.redirect("../options.html");
 }
 
-var preventBehavior = function() {
+function onMenuKeyDown() {
     // Handle the back button
 	navigator.notification.vibrate(1);
+	location.redirect("./options.html");
 }
-
 
 function init() {
-    document.addEventListener("touchmove", preventBehavior, false);
     document.addEventListener("deviceready", deviceReady, true);
 	document.addEventListener("menubutton", onMenuKeyDown, true);
 	
